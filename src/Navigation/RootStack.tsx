81c3b6bb2b8e2790df7189e4d48e9f1ec512a552
Samy Djemaï <53857555+SamyDjemai@users.noreply.github.com>
2020-12-28 18:07:37 +0100

    Added custom header buttons, edited assets

--- src/Navigation/RootStack.tsx
+++ src/Navigation/RootStack.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import { createStackNavigator } from "@react-navigation/stack";
+import { Feather } from '@expo/vector-icons';
 import LoginScreen from '../Components/LoginScreen';
 import NotesScreen from '../Components/NotesScreen';
+import { Button, Linking, StyleSheet, TouchableOpacity } from 'react-native';
 
 const Stack = createStackNavigator();
 
@@ -16,10 +18,24 @@ function RootStack() {
       <Stack.Screen
         name="NotesScreen"
         component={NotesScreen}
-        options={{ title: "Notes", headerBackTitle: "Déconnexion" }}
+        options={{
+          title: "Notes",
+          headerBackTitle: "Déconnexion",
+          headerBackImage: () => <Feather name="log-out" size={24} color="black" />,
+          headerRight: () =>
+            <TouchableOpacity style={styles.headerInfoButton} onPress={() => Linking.openURL("https://github.com/SamyDjemai/notes-imt-mobile")}>
+              <Feather name="info" size={24} color="black" />
+            </TouchableOpacity>,
+        }}
       />
     </Stack.Navigator>
   )
 }
+
+const styles = StyleSheet.create({
+  headerInfoButton: {
+    marginRight: 12
+  }
+})
 
 export default RootStack
